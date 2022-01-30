5c597c55d8f489aa7acd38f67d8aad00a31be5c7
Oyindamola Peterson <oyindamolapeterson@Oyindamolas-MacBook-Air.local>
2022-01-30 06:21:04 -0500

    updated the folders

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-README.md
+## User story
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+## Criteria from the user
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+## Description
+
+This project is designed for a user to be able to view a password after answering a series of prompts
+
+The user is prompted to answer the folllowing questions:
+
+- The user is prompted to enter the number of letters from 8-128,
+  ![enterpassword](../Password-creator-Javascript/Assets/enter.password.png)
+
+- The user is prompted to enter if they would want lowercase letters,
+  ![lowercase](../Password-creator-Javascript/Assets/req.lowercase.png)
+
+- The user is prompted to enter if they would want uppercase letters,
+  ![uppercase](../Password-creator-Javascript/Assets/req.lowercase.png)
+
+- The user is prompted to enter if they would want numbers,
+  ![numbercase](../Password-creator-Javascript/Assets/req.numbers.png)
+
+In the first question, if the user enters a number outside of the given range (8-128), the user is prompted with a wrong password length.
+
+![wrong-length](../Password-creator-Javascript/Assets/wrongpasswordlength.png)
+
+A passweod is displayed after the questions have been answered.
+
+Access:
+![password](../Password-creator-Javascript/Assets/password.png)
+
+## Links
+
+## Contact me
+
+[Github] (https://github.com/oyindami)
--- script.js
+++ script.js
@@ -1,33 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = "qwertyuiopasdfghjklzxcvbnm";
+var uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var special = "!#$%&()*+-./:;<=>?@^_`";
+var num = "1234567890";
+//variable to allow user code to concacnate different options
+var choices;
+//variable to save all tghe user true options
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-function generatepassword(){
-  var password = window.prompt("enter password length?");
-  var value;
-  
-  if(password.length < 8) {
-    alert("error!")
-  }
-  return('');
-
-  if()
-    
-  }
-// arrays for password options//
-var choices =[lowercase, numbers]
-var lowercase = 'qwertyuiopasdfghjklzxcvbnm'
-var uppercase = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-var numbers = '1234567890'
-choices [Math.floor(Math.random()*choices.length)
-}
-]
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatepassword);
+
+function generatepassword() {
+  var passwordlength = prompt(
+    "enter password length; choose between 8 and 128 character length"
+  );
+  if (passwordlength < 8 || passwordlength > 128) {
+    alert("Wrong password length");
+    return generatepassword();
+  } else {
+    var lowercasepassword = confirm("Require lowercase letters?");
+    var uppercasepassword = confirm("Require Uppercase letters?");
+    var numberspassword = confirm("Require numbers?");
+    var specialpassword = confirm("Require Special Characters?");
+  }
+  //the next step series of ifelse statement that based on the confirm statement above
+  // to display option when the user picks true for two options and concat choices creating an array
+  if (lowercasepassword && uppercasepassword) {
+    choices = lowercase.concat(uppercase);
+  } else if (numberspassword && uppercasepassword) {
+    choices = num.concat(uppercase);
+  } else if (numberspassword && specialpassword) {
+    choices = num.concat(special);
+  } else if (specialpassword && lowercasepassword) {
+    choices = special.concat(lowercase);
+  } else if (uppercasepassword && specialpassword) {
+    choices = uppercase.concat(special);
+  } else if (lowercasepassword && numberspassword) {
+    choices = lowercase.concat(num);
+  }
+  //else if statement to join option of three true choices
+  else if (lowercasepassword && uppercasepassword && numberspassword) {
+    choices = lowercase.concat(uppercase, num);
+  } else if (lowercasepassword && uppercasepassword && specialpassword) {
+    choices = lowercase.concat(uppercase, special);
+  } else if (uppercasepassword && numberspassword && specialpassword) {
+    choices = uppercase.concat(num, special);
+  }
+  //else if statement for option of user picking true for one option
+  else if (uppercasepassword) {
+    choices = uppercase;
+  } else if (lowercasepassword) {
+    choices = lowercase;
+  } else if (numberspassword) {
+    choices = num;
+  } else if (specialpassword) {
+    choices = special;
+  }
+
+  //loop statement to randomly pick between the choices options//
+
+  //made my random value gotten to be a string
+
+  var pickedchoices = "";
+  for (var i = 0; i < passwordlength; i++) {
+    pickedchoices += choices[Math.floor(Math.random() * choices.length)];
+  }
+  document.getElementById("password").placeholder = pickedchoices;
+  //alert (pickedchoices);
+}
+
+//document.getElementById("password"). placeholder = pickedchoices;
+//mkae sure to close for loop and indicate all choices random values are to be in a string
